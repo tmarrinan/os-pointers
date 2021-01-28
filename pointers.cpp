4e6ca554d986915ac303f59a73baff4498675c88
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2021-01-28 09:12:06 -0600

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,57 @@
+// compile: g++ -std=c++11 -o pointers pointers.cpp
+#include <iostream>
+#include <string>
+
+typedef struct Student {
+    int id;
+    char *f_name;
+    char *l_name;
+    int n_assignments;
+    double *grades;
+} Student;
+
+int promptInt(std::string message, int min, int max);
+double promptDouble(std::string message, double min, double max);
+void calculateStudentAverage(void *object, double *avg);
+
+int main(int argc, char **argv)
+{
+    Student student;
+    double average;
+
+    // Sequence of user input -> store in fields of `student`
+
+    // Call `CalculateStudentAverage(???, ???)`
+    // Output `average`
+
+    return 0;
+}
+
+/*
+   message: text to output as the prompt
+   min: minimum value to accept as a valid int
+   max: maximum value to accept as a valid int
+*/
+int promptInt(std::string message, int min, int max)
+{
+    // Code to prompt user for an int
+}
+
+/*
+   message: text to output as the prompt
+   min: minimum value to accept as a valid double
+   max: maximum value to accept as a valid double
+*/
+double promptDouble(std::string message, double min, double max)
+{
+    // Code to prompt user for a double
+}
+
+/*
+   object: pointer to anything - your choice! (but choose something that will be helpful)
+   avg: pointer to a double (can store a value here)
+*/
+void calculateStudentAverage(void *object, double *avg)
+{
+    // Code to calculate and store average grade
+}
